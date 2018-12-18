238cd3e4fe3e97d24d1f5a11dba052a1aeb414ff
qbhy <96qbhy@gmail.com>
2018-12-18 15:53:23 +0800

    修改config目录名

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/nfnt/resize"
 	"github.com/qbhy/go-utils"
-	"github.com/qbhy/poster-generater/Config"
+	"github.com/qbhy/poster-generater/config"
 	"net/http"
 	"os"
 )
