1793d4f2b1f930a3d801fa1990304bbfd02f0c93
qbhy <572490755@qq.com>
2019-05-02 22:51:40 +0800

    block支持boxSizing属性

--- src/main/java/com/qbhy/poster/drawable/Block.java
+++ src/main/java/com/qbhy/poster/drawable/Block.java
@@ -45,15 +45,29 @@ public class Block extends Drawable {
      */
     private int borderWidth = 0;
 
+    /**
+     * 盒子模型,支持 contentBox 或者 borderBox
+     */
+    private String boxSizing = "borderBox";
+
     @Override
-    public void draw(Graphics2D gd) throws IOException {
+    public void draw(Graphics2D gd) {
+
+        Integer offset = 0;
+
+        if (!boxSizing.equals("borderBox")) {
+            offset = borderWidth;
+        }
+
         if (backgroudColor != null) {
             gd.setColor(ColorTools.String2Color(backgroudColor)); // 设置画笔颜色
-            gd.fillRect(x, y, width, height); // 画填充矩形
-        } else if (borderWidth > 0) {
+            gd.fillRect(x + offset, y + offset, width, height); // 画填充矩形
+        }
+
+        if (borderWidth > 0) {
             gd.setStroke(new BasicStroke((float) borderWidth)); // 设置画笔大小
             gd.setColor(ColorTools.String2Color(borderColor)); // 设置画笔颜色
-            gd.drawRect(x, y, width, height); // 画边框矩形
+            gd.drawRect(x + borderWidth / 2, y + borderWidth / 2, width - borderWidth + offset * 2, height - borderWidth + offset * 2); // 画边框矩形
         }
     }
 
